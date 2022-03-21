13a37e085c62d2f2d985ebeb161e2173b9065ec1
MarcoServinDLT <marco.servin@alumnos.udg.mx>
2022-03-21 12:59:38 -0600

    Programa principal del problema Santa Claus

--- main.py
+++ main.py
@@ -1,17 +1,54 @@
+from ctypes import resize
 import tkinter
 import threading
+from PIL import ImageTk, Image
 from time import sleep
 
-# Variabes globales y semaforos. #
+# Variabes globales y semafóros. #
 MAX_ELFS = 3
 MAX_REINDEERS = 7
 elfs = 0
 reindeers = 0
 elfs_sem = threading.Semaphore()
-reindeer_sem = threading.Semaphore()
-mutex = threading.Semaphore(1)
+reindeer_sem = threading.Semaphore(MAX_REINDEERS)
+mutex = threading.Semaphore()
 santa_sem = threading.Semaphore()
 elfs_mutex = threading.Semaphore()
+
+# Inicialización de la interfaz gráfica
+App = tkinter.Tk()
+App.title("Santa Claus 🎅")
+App.geometry("700x350")
+
+# Se cargan las impagenes de los duendes.
+elf_img = ImageTk.PhotoImage(Image.open("res/santa.jpg"))
+
+# Imégen en la app correspondiente a los duendes.
+elf_gui = tkinter.Label(App, image = elf_img)
+elf_gui.grid(row = 0, column = 0)
+
+# Etiqueta del conteo de duendes.
+elfs_cont = tkinter.StringVar()
+elf_lb = tkinter.Label(App, textvariable = elfs_cont).grid(row = 1, column = 0)
+
+
+# -------------------- Funciones para modificar la interfaz grafica. -------------------- #
+
+# Función de solicitud de ayuda de los duendes.
+def elfs_get_help() -> None:
+    pass
+
+# Función para atender a los procesos renos.
+def prepare_sled() -> None:
+    pass
+
+# Función para definir que los regalos se repartieron.
+def gifts() -> None:
+    pass
+
+# Función para domir a santa.
+def back_to_sleep() -> None:
+    elf_gui.configure(image = ches_img)
 
 # Función para el proceso de santa claus. #
 def santa_process() -> None:
@@ -22,15 +59,20 @@ def santa_process() -> None:
         mutex.acquire()
         if reindeers == MAX_REINDEERS:
             print("Preparando trineo...")
+            # Liberación de los procesos de los renos.
             while reindeers:
                 reindeers -= 1
                 reindeer_sem.release()
             sleep(3)
         elif elfs == MAX_ELFS:
             print("Santa ayuda a los duendes...")
-            for i in range(MAX_ELFS):
-                elfs_sem.release()
+            # Liberación de los duendes.
+            while elfs:
+                elfs -= 1
+                sleep(1)
+            sleep(1)
         mutex.release()
+        back_to_sleep()
 
 # Función para el proceso de los duendes. #
 def elf_process() -> None:
@@ -44,10 +86,9 @@ def elf_process() -> None:
         else:
             elfs_mutex.release()
         mutex.release()
-        print("número de duendes:", elfs)
-        elfs_sem.acquire()
+        elfs_cont.set(f"{elfs} Duendes trabajando")
+        sleep(1)
         mutex.acquire()
-        elfs -= 1
         if not elfs:
             elfs_mutex.release()
         mutex.release()
@@ -68,10 +109,12 @@ def reindeer_process() -> None:
 
 # Ejecución principal del programa.
 if __name__ == "__main__":
-    santa_thread = threading.Thread(target=santa_process)
-    elf_threads = threading.Thread(target=elf_process)
-    reindeer_threads = threading.Thread(target=reindeer_process)
+    santa_thread = threading.Thread(target = santa_process)
+    elf_threads = threading.Thread(target = elf_process)
+    reindeer_threads = threading.Thread(target = reindeer_process)
 
     santa_thread.start()
     elf_threads.start()
     reindeer_threads.start()
+
+    App.mainloop()
